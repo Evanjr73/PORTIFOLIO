f50e5c121a790563f93f19bbdab9bda1cc8d16d0
Evan junior <evsioljr@gmail.com>
2022-10-14 12:52:26 -0300

    segundo comit

--- vite-project/src/twet.tsx
+++ vite-project/src/twet.tsx
@@ -6,8 +6,8 @@ export function Twet(){
             <div id='container'>
                 <h1 id='titulo'>Programador front-end</h1>
 
-                <div id='container2'>
-                <a href=""> contato</a>
+                <div id='nav'>
+                    <a href=""> CONTATO</a>
                 </div>
 
             </div>
