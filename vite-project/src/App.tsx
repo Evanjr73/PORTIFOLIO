c90a46cb96372ee216bb845af83efa36c47444eb
Evan junior <evsioljr@gmail.com>
2022-10-14 10:22:19 -0300

    1 primeiro comit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Twet } from './twet'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Twet/>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/twet.tsx
@@ -0,0 +1,17 @@
+import './twet.css'
+
+export function Twet(){
+    return(
+        <>
+            <div id='container'>
+                <h1 id='titulo'>Programador front-end</h1>
+
+                <div id='container2'>
+                <a href=""> contato</a>
+                </div>
+
+            </div>
+            
+        </>
+    )
+}
