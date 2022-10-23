2c16ac6cfe2dce98d017f8bf11edbac94443c3ba
Evan junior <evsioljr@gmail.com>
2022-10-22 22:14:50 -0300

    melhorando

--- vite-project/src/texto.tsx
+++ vite-project/src/texto.tsx
@@ -1,10 +1,10 @@
 import './texto.css'
-import img from './img/perfil.png' 
+import img from './img/perfil2.jpeg' 
 
 export function Texto(){
     return(
         <div id='divtext'>
-            <div>
+            <div id='divtext2'>
                 <img src={img} alt="" />
                 <h1>EVAN JUNIOR</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ullam voluptatibus quidem voluptates. Architecto voluptate natus provident repellat sit dolor dicta asperiores. Voluptates ad, eius est temporibus harum quis vitae Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, esse fuga? Eos inventore voluptatem sapiente, ullam magni reprehenderit voluptas minima dolorem deleniti sint ut possimus vitae nesciunt nemo temporibus dolore!</p>
