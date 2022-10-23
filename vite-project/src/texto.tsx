5b70bbaf30ac446efa9f8bd16004a5868952b029
Evan junior <evsioljr@gmail.com>
2022-10-23 19:50:07 -0300

    melhorando localização do texto

--- vite-project/src/texto.tsx
+++ vite-project/src/texto.tsx
@@ -4,14 +4,16 @@ import img from './img/perfil2.jpeg'
 export function Texto(){
     return(
         <div id='divtext'>
-            <div id='divtext2'>
-                <img src={img} alt="" />
-                <h1>EVAN JUNIOR</h1>
-                
-                
-            </div>
-            <div id="texto">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ullam voluptatibus quidem voluptates. Architecto voluptate natus provident repellat sit dolor dicta asperiores. Voluptates ad, eius est temporibus harum quis vitae Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, esse fuga? Eos inventore voluptatem sapiente, ullam magni reprehenderit voluptas minima dolorem deleniti sint ut possimus vitae nesciunt nemo temporibus dolore!</p>
+            <div id='divtext3'>
+                <div id='divtext2'>
+                    <img src={img} alt="" />
+                    <h1>EVAN JUNIOR</h1>
+                    
+                    
+                </div>
+                <div id="texto">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ullam voluptatibus quidem voluptates. Architecto voluptate natus provident repellat sit dolor dicta asperiores. Voluptates ad, eius est temporibus harum quis vitae Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, esse fuga? Eos inventore voluptatem sapiente, ullam magni reprehenderit voluptas minima dolorem deleniti sint ut possimus vitae nesciunt nemo temporibus dolore!</p>
+                </div>
             </div>
         
         </div>
