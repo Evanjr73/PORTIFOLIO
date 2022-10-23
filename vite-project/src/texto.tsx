4bf556bd8133feb8d9086da7f482aa77e7025730
Evan junior <evsioljr@gmail.com>
2022-10-23 19:36:19 -0300

    melhorando o text

--- vite-project/src/texto.tsx
+++ vite-project/src/texto.tsx
@@ -7,11 +7,11 @@ export function Texto(){
             <div id='divtext2'>
                 <img src={img} alt="" />
                 <h1>EVAN JUNIOR</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ullam voluptatibus quidem voluptates. Architecto voluptate natus provident repellat sit dolor dicta asperiores. Voluptates ad, eius est temporibus harum quis vitae Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, esse fuga? Eos inventore voluptatem sapiente, ullam magni reprehenderit voluptas minima dolorem deleniti sint ut possimus vitae nesciunt nemo temporibus dolore!</p>
+                
                 
             </div>
             <div id="texto">
-                
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ullam voluptatibus quidem voluptates. Architecto voluptate natus provident repellat sit dolor dicta asperiores. Voluptates ad, eius est temporibus harum quis vitae Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, esse fuga? Eos inventore voluptatem sapiente, ullam magni reprehenderit voluptas minima dolorem deleniti sint ut possimus vitae nesciunt nemo temporibus dolore!</p>
             </div>
         
         </div>
