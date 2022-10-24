8f93e0ac1b9f5f5bd3e006334aea2ba7528cf0ac
Evan junior <evsioljr@gmail.com>
2022-10-24 20:04:24 -0300

    melhorando box-shadow da img

--- vite-project/src/projetos.tsx
+++ vite-project/src/projetos.tsx
@@ -20,41 +20,78 @@ export function Projetos(){
             <h1 id='projetosh1'>projetos</h1>
         </div>
         <div id='containerProjetos1'>
-
-            
-            
-            <div className='divprojetos'>
-                <img src={img} alt="" className='projetos'/> 
-                
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum hic inventore excepturi rem, quia soluta alias quis perspiciatis dolorum accusantium quibusdam, recusandae nam doloribus. Voluptatibus possimus repellendus atque dolorem harum.</p>
-            
-            </div>
-            <div  className='divprojetos'>
-                <img src={img1} alt=""  className='projetos'/>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum hic inventore excepturi rem, quia soluta alias quis perspiciatis dolorum accusantium quibusdam, recusandae nam doloribus. Voluptatibus possimus repellendus atque dolorem harum.</p>
-            </div>
+            <a href="https://github.com/Evanjr73/POKEMON"  target="_blank">
+                <div  className='divprojetos'>
+                    <img src={img} alt="" className='projetos'/>
+                    <div className='textoDoProjeto'>
+                        <h2>PROJETO POKEMON</h2> <br />
+                        <p>PORJETO CRIADO COM HTML , CSS JAVASCRIPT CONSUMINDO UMA API DE TERCEIROS</p>
+                    </div>
+                    
+                </div>
+            </a>
 
 
-            <div  className='divprojetos'>
-                <img src={img2} alt=""  className='projetos'/>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum hic inventore excepturi rem, quia soluta alias quis perspiciatis dolorum accusantium quibusdam, recusandae nam doloribus. Voluptatibus possimus repellendus atque dolorem harum.</p>
-            </div>
-            <div  className='divprojetos'>
-                <img src={img3} alt=""  className='projetos'/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, tempora similique facere corrupti nostrum enim. Praesentium eos tempore ab a dolore doloremque fugit suscipit qui optio dolorum, molestias porro nulla!</p>
-            </div>
-            <div  className='divprojetos'>
-                <img src={img4} alt="" className='projetos'/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis omnis quidem cupiditate autem ullam voluptatem nesciunt fuga debitis quod nobis explicabo necessitatibus soluta fugiat sit veritatis, sunt, earum atque porro.</p>
-            </div>
-            <div  className='divprojetos'>
-                <img src={img5} alt="" className='projetos'/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis omnis quidem cupiditate autem ullam voluptatem nesciunt fuga debitis quod nobis explicabo necessitatibus soluta fugiat sit veritatis, sunt, earum atque porro.</p>
-            </div>
-            <div  className='divprojetos'>
-                <img src={img6} alt="" className='projetos'/>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis omnis quidem cupiditate autem ullam voluptatem nesciunt fuga debitis quod nobis explicabo necessitatibus soluta fugiat sit veritatis, sunt, earum atque porro.</p>
-            </div>
+
+
+            <a href="https://github.com/Evanjr73/POKEMON"  target="_blank">
+                <div  className='divprojetos'>
+                    <img src={img2} alt="" className='projetos'/>
+                    <div className='textoDoProjeto'>
+                        <h2>PROJETO POKEMON</h2> <br />
+                        <p>PORJETO CRIADO COM HTML , CSS JAVASCRIPT CONSUMINDO UMA API DE TERCEIROS</p>
+                    </div>
+                    
+                </div>
+            </a>
+
+
+
+            <a href="https://github.com/Evanjr73/POKEMON"  target="_blank">
+                <div  className='divprojetos'>
+                    <img src={img3} alt="" className='projetos'/>
+                    <div className='textoDoProjeto'>
+                        <h2>PROJETO POKEMON</h2> <br />
+                        <p>PORJETO CRIADO COM HTML , CSS JAVASCRIPT CONSUMINDO UMA API DE TERCEIROS</p>
+                    </div>
+                    
+                </div>
+            </a>
+
+            <a href="https://github.com/Evanjr73/POKEMON"  target="_blank">
+                <div  className='divprojetos'>
+                    <img src={img4} alt="" className='projetos'/>
+                    <div className='textoDoProjeto'>
+                        <h2>PROJETO POKEMON</h2> <br />
+                        <p>PORJETO CRIADO COM HTML , CSS JAVASCRIPT CONSUMINDO UMA API DE TERCEIROS</p>
+                    </div>
+                    
+                </div>
+            </a>
+
+
+
+            <a href="https://github.com/Evanjr73/POKEMON"  target="_blank">
+                <div  className='divprojetos'>
+                    <img src={img5} alt="" className='projetos'/>
+                    <div className='textoDoProjeto'>
+                        <h2>PROJETO POKEMON</h2> <br />
+                        <p>PORJETO CRIADO COM HTML , CSS JAVASCRIPT CONSUMINDO UMA API DE TERCEIROS</p>
+                    </div>
+                    
+                </div>
+            </a>
+            
+            <a href="https://github.com/Evanjr73/POKEMON"  target="_blank">
+                <div  className='divprojetos'>
+                    <img src={img6} alt="" className='projetos'/>
+                    <div className='textoDoProjeto'>
+                        <h2>PROJETO POKEMON</h2> <br />
+                        <p>PORJETO CRIADO COM HTML , CSS JAVASCRIPT CONSUMINDO UMA API DE TERCEIROS</p>
+                    </div>
+                    
+                </div>
+            </a>
             <a href="https://github.com/Evanjr73/POKEMON"  target="_blank">
                 <div  className='divprojetos'>
                     <img src={img7} alt="" className='projetos'/>
